b9c0a65f71ec486e03d8bfa603101ef11979c676
Sunshoyen <96290162+Sunshoyen@users.noreply.github.com>
2022-06-16 17:42:39 +0700

    Add files via upload

---
+++ sushi/js/tel__number.js
@@ -0,0 +1,19 @@
+const input = document.getElementById('only_num');
+
+input.addEventListener('keydown', function(event) {
+	// Разрешаем: backspace, delete, tab и escape
+	if ( event.keyCode == 46 || event.keyCode == 8 || event.keyCode == 9 || event.keyCode == 27 ||
+		// Разрешаем: Ctrl+A
+		(event.keyCode == 65 && event.ctrlKey === true) ||
+		// Разрешаем: home, end, влево, вправо
+		(event.keyCode >= 35 && event.keyCode <= 39)) {
+		
+		// Ничего не делаем
+		return;
+	} else {
+		// Запрещаем все, кроме цифр на основной клавиатуре, а так же Num-клавиатуре
+		if ((event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105 )) {
+			event.preventDefault();
+		}
+	}
+});
